2ff16bd5cc3a3aae0f04ac1fc02feab7e259a03e
Gabituts <gabrielbastias25@gmail.com>
2023-11-21 19:01:54 -0300

    navbar listo

--- src/app/pages/compra/compra.module.ts
+++ src/app/pages/compra/compra.module.ts
@@ -7,13 +7,15 @@ import { IonicModule } from '@ionic/angular';
 import { CompraPageRoutingModule } from './compra-routing.module';
 
 import { CompraPage } from './compra.page';
+import { SharedModule } from 'src/app/shared/shared.module';
 
 @NgModule({
   imports: [
     CommonModule,
     FormsModule,
     IonicModule,
-    CompraPageRoutingModule
+    CompraPageRoutingModule,
+    SharedModule
   ],
   declarations: [CompraPage]
 })
