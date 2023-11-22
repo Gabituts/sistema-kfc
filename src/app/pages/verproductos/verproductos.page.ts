0bc6974530e7972c96341292939a9a453ff5e166
Gabituts <gabrielbastias25@gmail.com>
2023-11-22 05:44:24 -0300

    inicio vinculado a bd

--- src/app/pages/verproductos/verproductos.page.ts
+++ src/app/pages/verproductos/verproductos.page.ts
@@ -1,15 +1,34 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, inject } from '@angular/core';
+import { FormGroup, FormControl, Validators } from '@angular/forms';
+import { Producto } from 'src/app/models/producto.models';
+import { FirebaseService } from 'src/app/services/firebase.service';
+import { UtilsService } from 'src/app/services/utils.service';
 
 @Component({
   selector: 'app-verproductos',
   templateUrl: './verproductos.page.html',
   styleUrls: ['./verproductos.page.scss'],
 })
-export class VerproductosPage implements OnInit {
+export class VerproductosPage {
 
-  constructor() { }
+  firebaseService = inject(FirebaseService)
+  utilsService = inject(UtilsService)
 
-  ngOnInit() {
+  ionViewWillEnter(){
+    this.getProducts()
   }
 
+//FUNCION PARA OBTENER PRODUCTOS
+productos: Producto[] = [];
+
+  getProducts(){
+    let path = `productos`
+    let sub = this.firebaseService.getColeccionData(path).subscribe({
+      next: (res:any) =>{
+        console.log(res)
+        this.productos = res
+        sub.unsubscribe()
+      }
+    })
+  }
 }
