9860a646ec58e83b3720b67a071fffb45e754d69
Gabituts <gabrielbastias25@gmail.com>
2023-11-21 21:36:49 -0300

    fuinciona logeo

--- src/app/pages/auth/login/login.page.ts
+++ src/app/pages/auth/login/login.page.ts
@@ -1,5 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, inject } from '@angular/core';
 import { FormGroup, FormControl, Validators } from '@angular/forms';
+import { User } from 'src/app/models/users.model';
+import { FirebaseService } from 'src/app/services/firebase.service';
 
 @Component({
   selector: 'app-login',
@@ -8,21 +10,18 @@ import { FormGroup, FormControl, Validators } from '@angular/forms';
 })
 export class LoginPage {
 
-  // firebaseService = inject(FirebaseService)
+  firebaseService = inject(FirebaseService)
 
   form = new FormGroup({
     email: new FormControl('', [Validators.required, Validators.email]),
     password: new FormControl('', [Validators.required]),
   })
 
-  constructor() { }
-
-
   ingresar(){
     console.log(this.form.value)
-    // this.firebaseService.IniciarSesion(this.form.value as User).then(res =>{
-    //   console.log(res)
-    // })
+    this.firebaseService.IniciarSesion(this.form.value as User).then(res =>{
+      console.log(res)
+    })
   }
 
 
