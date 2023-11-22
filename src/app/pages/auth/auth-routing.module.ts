45ee01ab6d7bb1ac89c5a81c0bc496ab57177dc0
Gabituts <gabrielbastias25@gmail.com>
2023-11-22 01:01:19 -0300

    funciona registrar usuarios con nombre y todo

--- src/app/pages/auth/auth-routing.module.ts
+++ src/app/pages/auth/auth-routing.module.ts
@@ -11,7 +11,12 @@ const routes: Routes = [
   {
     path: 'login',
     loadChildren: () => import('./login/login.module').then( m => m.LoginPageModule)
+  },
+  {
+    path: 'registrarse',
+    loadChildren: () => import('./register/register.module').then( m => m.RegisterPageModule)
   }
+
 ];
 
 @NgModule({
