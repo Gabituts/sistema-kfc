2b0b86382a23e99546019d0d0fd145320fced770
Gabituts <gabrielbastias25@gmail.com>
2023-11-21 21:51:43 -0300

    fuinciona error de logueo

--- src/app/pages/auth/login/login.page.ts
+++ src/app/pages/auth/login/login.page.ts
@@ -2,6 +2,7 @@ import { Component, OnInit, inject } from '@angular/core';
 import { FormGroup, FormControl, Validators } from '@angular/forms';
 import { User } from 'src/app/models/users.model';
 import { FirebaseService } from 'src/app/services/firebase.service';
+import { UtilsService } from 'src/app/services/utils.service';
 
 @Component({
   selector: 'app-login',
@@ -11,18 +12,32 @@ import { FirebaseService } from 'src/app/services/firebase.service';
 export class LoginPage {
 
   firebaseService = inject(FirebaseService)
+  utilsService = inject(UtilsService)
 
   form = new FormGroup({
     email: new FormControl('', [Validators.required, Validators.email]),
     password: new FormControl('', [Validators.required]),
   })
 
-  ingresar(){
+  async ingresar(){
     console.log(this.form.value)
+    
+    //cargando
+    const loading = await this.utilsService.loading();
+    await loading.present();
+
     this.firebaseService.IniciarSesion(this.form.value as User).then(res =>{
       console.log(res)
+    }).catch(error =>{
+      console.log(error);
+      this.utilsService.presentToast({
+        message: error.message,
+        duration: 2500,
+        color: 'primary',
+        position: 'middle'
+      })
+    }).finally(() => {
+      loading.dismiss();
     })
   }
-
-
 }
--- src/app/services/utils.service.ts
+++ src/app/services/utils.service.ts
@@ -1,9 +1,23 @@
-import { Injectable } from '@angular/core';
+import { Injectable, inject } from '@angular/core';
+import { LoadingController, ToastController, ToastOptions } from '@ionic/angular';
 
 @Injectable({
   providedIn: 'root'
 })
 export class UtilsService {
 
-  constructor() { }
+  loadingCtrl = inject(LoadingController)
+  toastCtrl = inject(ToastController)
+
+  //LOADING
+  loading(){
+    return this.loadingCtrl.create({spinner: 'crescent'})
+  }
+
+  //ERROR ALERTA
+  async presentToast(opts?: ToastOptions){
+    const toast = await this.toastCtrl.create(opts);
+    toast.present()
+  }
+
 }
